da48533a1c6a493ede3947802183d7314e1858d8
junyu.wy <junyu.wy@taobao.com>
2013-12-02 17:36:48 +0800

    fix

--- mould_pc.php
+++ mould_pc.php
@@ -5,7 +5,6 @@ require 'basicDB.php';
 require 'MyDB.class.php';
 require 'MessageDO.class.php';
 Globle::initSmarty();
-require 'is_login.php';
 $messageDO = MyDB::selectInfoDB($user_id,$link);
 if($messageDO!=null){
 	Globle::$smarty->assign("pcmould",$messageDO->pc_mould);
