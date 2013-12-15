fe79b546613fc5a51f7a04b20329378d51a3b762
wybest <wy@870629>
2013-12-15 20:54:44 +0800

    添加目录

--- upfile1.php
+++ upfile1.php
@@ -19,7 +19,7 @@ if($responseCore->isOK()){
 //    for ($i = 0; $i < $lengh; $i++){
 //        $dt_record = $object_list[$i];
 //// 		echo $dt_record->object."</br>";
-//        $fileWriteTo = "./marryimg/";
+        $fileWriteTo = "./marryimg/2013-12-15-16-27-28.jpg";
 //
 //        $opt = array (
 //            "fileWriteTo" => $fileWriteTo.$dt_record->object );
@@ -33,8 +33,8 @@ if($responseCore->isOK()){
     echo $lengh;
 
     $opt = array (
-        "fileWriteTo" => $fileWriteTo. "2013-12-15-16-27-28.jpg" );
-    $response = $baiduBCS->get_object ( $bucket, "2013-12-15-16-27-28.jpg", $opt );
+        "fileWriteTo" => $fileWriteTo);
+    $response = $baiduBCS->get_object ( $bucket, "hhh.jpg", $opt );
     if (! $response->isOK ()) {
         die ( "Download object failed." );
     }
