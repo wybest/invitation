46fcd7ee490e3b9d03770c9468d9bf2a94610c2f
wybest <wy@870629>
2013-12-22 22:19:35 +0800

    fix

--- new_save_music.php
+++ new_save_music.php
@@ -9,22 +9,7 @@ require 'is_login.php';
 
 if($_REQUEST['insert'] == "insert"){
 	$music=$_REQUEST['music'];
-	$target = false;
-	$messageDO = MyDB::selectInfoDB($user_id,$link);
 
-	if($messageDO==null){
-		$message = "请先完成 “请柬内容设置”";
-		Globle::$smarty->assign("message",$message);
-		Globle::$smarty->display('new_music.tpl');
-		die();
-	}else{
-		$target = MyDB::updateInfoMusicDB($music,$user_id,$link);
-	}
-	if($target){
-		Globle::$smarty->assign("target","true");
-	}else{
-		Globle::$smarty->assign("target","false");
-	}
 	//初始化音乐列表
 	require 'init_mp3list.php';
 
