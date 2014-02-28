d9d6bfdac1c93856a0fc5e951028b424ef94c9f2
君玉 <junyu.wy@taobao.com>
2014-02-28 22:22:13 +0800

    fix

--- style/style17/mimg.1.8.js
+++ style/style17/mimg.1.8.js
@@ -196,16 +196,16 @@ $(function(){
 		$(this).toggleClass("xs_border_top").prev(".xh").stop().slideToggle("slow");
 	})
 	
-//	$(window).scroll( function() {
-//	    var scroll_top=parseInt( $(window).scrollTop());
-//	    var msgTop= parseInt($(".bd_msg").offset().top);
-//
-//	    if(scroll_top>msgTop){
-//	            $(".xs_msg").removeClass("relative_css").addClass("fiexd_css");
-//	        }else{
-//
-//	            $(".xs_msg").removeClass("fiexd_css").addClass("relative_css");
-//
-//	    }
-//	} );
+	$(window).scroll( function() {
+	    var scroll_top=parseInt( $(window).scrollTop());
+	    var msgTop= parseInt($(".bd_msg").offset().top);
+
+	    if(scroll_top>msgTop){
+	            $(".xs_msg").removeClass("relative_css").addClass("fiexd_css");
+	        }else{
+
+	            $(".xs_msg").removeClass("fiexd_css").addClass("relative_css");
+
+	    }
+	} );
 })
