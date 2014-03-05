8d15ad7e1b596f86b9129dda5a7956c7e8d1eaff
君玉 <junyu.wy@taobao.com>
2014-03-05 17:23:55 +0800

    fix

--- phone.php
+++ phone.php
@@ -165,7 +165,6 @@ function isMobile() {
                 					'windows phone',
                                     'trident',
                                     'presto',
-                                    'mac',
                                     'sec',
                                     'samsung'
             );
