060b794b37ebcb49229cf7ef7948445264a0b246
君玉 <junyu.wy@taobao.com>
2014-04-04 12:41:47 +0800

    fix

--- init_mp3list.php
+++ init_mp3list.php
@@ -183,6 +183,9 @@ $filesId[45] = "46.mp3";
 $files[45] = "Cuppy Cake Song.mp3";//47
 $filesId[45] = "47.mp3";
 
+$files[45] = "夫妻相.mp3";//48
+$filesId[45] = "48.mp3";
+
 Globle::$smarty->assign("mp3list",$files);
 Globle::$smarty->assign("mp3listId",$filesId);
 
