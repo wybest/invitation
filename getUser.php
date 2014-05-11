fa40a0b9b5fa378bc2d2073a313382fdc1581d7c
junyu.wy <junyu.wy@taobao.com>
2014-05-11 12:41:57 +0800

    fix

---
+++ getUser.php
@@ -0,0 +1,15 @@
+<?php
+require 'smarty/libs/Smarty.class.php';
+require 'Globle.class.php';
+require 'basicDB.php';
+require 'MyDB.class.php';
+require 'MessageDO.class.php';
+require 'VipDO.class.php';
+require 'UserDO.class.php';
+require 'ShujuDO.class.php';
+Globle::initSmarty();
+
+echo $_SERVER['HTTP_USER_AGENT'];
+
+
+?>
